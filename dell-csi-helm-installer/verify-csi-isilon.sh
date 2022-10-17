fd663b625f5b7bc8da1c2a595184c0a823eff944
shefali-malhotra <91597668+shefali-malhotra@users.noreply.github.com>
2022-10-18 00:20:04 +0530

    Changes for standalone helm chart support (#139)
    * Changes for standalone helm chart support
    
    * Corrected version in chart.yaml
    
    Added k8s1.25 in helper.tpl
    
    * addresed review comments

--- dell-csi-helm-installer/verify-csi-isilon.sh
+++ dell-csi-helm-installer/verify-csi-isilon.sh
@@ -19,6 +19,6 @@ function verify-csi-isilon() {
   verify_alpha_snap_resources
   verify_snap_requirements
   verify_helm_3
-  verify_helm_values_version "2.5.0"
+  verify_helm_values_version "${DRIVER_VERSION}"
   verify_authorization_proxy_server
 }
