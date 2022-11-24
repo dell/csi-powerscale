ef2c386a77409e27f453ca82d4c0e8a14653ba78
Nitesh-Rewatkar <90749010+nitesh3108@users.noreply.github.com>
2022-11-24 13:55:20 +0530

    Enable OpenShift 4.11 support (#154)

--- dell-csi-helm-installer/verify-csi-isilon.sh
+++ dell-csi-helm-installer/verify-csi-isilon.sh
@@ -12,7 +12,7 @@
 # verify-csi-isilon method
 function verify-csi-isilon() {
   verify_k8s_versions "1.21" "1.25"
-  verify_openshift_versions "4.9" "4.10"
+  verify_openshift_versions "4.10" "4.11"
   verify_namespace "${NS}"
   verify_required_secrets "${RELEASE}-creds"
   verify_optional_secrets "${RELEASE}-certs"
