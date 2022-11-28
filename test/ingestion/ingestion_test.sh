6cd442d28a15bb448c149a72e2bf984531bd72de
Nitesh-Rewatkar <90749010+nitesh3108@users.noreply.github.com>
2022-11-28 17:48:50 +0530

    Update Copyright information (#156)

--- test/ingestion/ingestion_test.sh
+++ test/ingestion/ingestion_test.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-# Copyright: (c) 2020, Dell EMC
+# Copyright © 2020-2021 Dell Inc. or its subsidiaries. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#      http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
 
 # volumename  volumehandle  storageclass  accessmode  storagesize   pvname   pvcname  clustername
 #     1             2            3             4            5         6         7          8
