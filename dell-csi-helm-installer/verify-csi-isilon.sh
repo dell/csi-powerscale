390458a8857d0dea3b4ad5df984cf0014a62fc0b
Nitesh-Rewatkar <90749010+nitesh3108@users.noreply.github.com>
2023-04-17 16:12:16 +0530

    2.7 commits (#186)

--- dell-csi-helm-installer/verify-csi-isilon.sh
+++ dell-csi-helm-installer/verify-csi-isilon.sh
@@ -14,8 +14,8 @@
 #
 # verify-csi-isilon method
 function verify-csi-isilon() {
-  verify_k8s_versions "1.21" "1.26"
-  verify_openshift_versions "4.10" "4.11"
+  verify_k8s_versions "1.21" "1.27"
+  verify_openshift_versions "4.11" "4.12"
   verify_namespace "${NS}"
   verify_required_secrets "${RELEASE}-creds"
   verify_optional_secrets "${RELEASE}-certs"
