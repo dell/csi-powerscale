2ec494873401d170420b112e1a464c3da6354bab
Kaleb Eckles <kalebeckles@gmail.com>
2024-03-22 14:06:49 -0400

    update OCP verification versions to match documentation
    Documented here that v2.10.0 is only supported on OCP 4.13 and 4.14: https://dell.github.io/csm-docs/docs/prerequisites/#supported-container-orchestrator-platforms

--- dell-csi-helm-installer/verify-csi-isilon.sh
+++ dell-csi-helm-installer/verify-csi-isilon.sh
@@ -15,7 +15,7 @@
 # verify-csi-isilon method
 function verify-csi-isilon() {
   verify_k8s_versions "1.21" "1.28"
-  verify_openshift_versions "4.12" "4.13"
+  verify_openshift_versions "4.13" "4.14"
   verify_namespace "${NS}"
   verify_required_secrets "${RELEASE}-creds"
   verify_optional_secrets "${RELEASE}-certs"
