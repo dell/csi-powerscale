18d034b633216cc861ae5a8e53f1c34cf52429b4
Daniyal Iqbal <86933570+daniyaliqbal2024@users.noreply.github.com>
2024-10-02 11:39:09 -0500

    Adding new k8s version support in helm-chart & csm-operator (#280)
    Co-authored-by: abhi16394 <32352976+abhi16394@users.noreply.github.com>

--- dell-csi-helm-installer/verify-csi-isilon.sh
+++ dell-csi-helm-installer/verify-csi-isilon.sh
@@ -14,8 +14,8 @@
 #
 # verify-csi-isilon method
 function verify-csi-isilon() {
-  verify_k8s_versions "1.21" "1.30"
-  verify_openshift_versions "4.15" "4.16"
+  verify_k8s_versions "1.25" "1.31"
+  verify_openshift_versions "4.16" "4.17"
   verify_namespace "${NS}"
   verify_required_secrets "${RELEASE}-creds"
   verify_optional_secrets "${RELEASE}-certs"
