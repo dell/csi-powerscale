30f3d74dabaaee8de872f10b643e1aad9f95cff7
Wilson Radadia <wilson.radadia@dell.com>
2025-01-06 12:29:03 +0530

    Update for the minimum version support for K8s and OCP

--- dell-csi-helm-installer/verify-csi-isilon.sh
+++ dell-csi-helm-installer/verify-csi-isilon.sh
@@ -14,8 +14,8 @@
 #
 # verify-csi-isilon method
 function verify-csi-isilon() {
-  verify_k8s_versions "1.25" "1.32"
-  verify_openshift_versions "4.16" "4.18"
+  verify_k8s_versions "1.26" "1.32"
+  verify_openshift_versions "4.17" "4.18"
   verify_namespace "${NS}"
   verify_required_secrets "${RELEASE}-creds"
   verify_optional_secrets "${RELEASE}-certs"
