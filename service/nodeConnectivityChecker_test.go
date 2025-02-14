db405338ee24619dc05df64bb90cbd969f747fa9
panigs7 <spandita.panigrahi@dell.com>
2025-02-14 02:25:07 -0500

    Improve UT Coverage for nodeconnectivitychecker

--- service/nodeConnectivityChecker_test.go
+++ service/nodeConnectivityChecker_test.go
@@ -1,12 +1,14 @@
 package service
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -31,16 +33,69 @@ func TestNodeHealth(t *testing.T) {
 }
 
 func TestConnectivityStatus(t *testing.T) {
-	arrayId := "array1"
+	arrayID := "array1"
 	status := ArrayConnectivityStatus{
 		LastSuccess: time.Now().Unix(),
 		LastAttempt: time.Now().Unix(),
 	}
-	probeStatus.Store(arrayId, status)
+	probeStatus.Store(arrayID, status)
 	// Test case: Successful response
 	w := httptest.NewRecorder()
 	connectivityStatus(w, httptest.NewRequest("GET", "/connectivity-status", nil))
 	assert.Equal(t, http.StatusOK, w.Code)
 	assert.Equal(t, "application/json", w.Header().Get("Content-Type"))
 	assert.True(t, strings.Contains(w.Body.String(), "array1"))
+}
+
+func TestGetArrayConnectivityStatus(t *testing.T) {
+	// Setup router
+	router := mux.NewRouter()
+	router.HandleFunc("/arrayStatus/{arrayId}", getArrayConnectivityStatus).Methods("GET")
+
+	// Test cases
+	tests := []struct {
+		arrayID       string
+		expectedCode  int
+		expectedBody  string
+		mockStatus    interface{}
+		mockStatusSet bool
+		isJSON        bool
+	}{
+		{
+			arrayID:       "existingArray",
+			expectedCode:  http.StatusOK,
+			expectedBody:  `{"status":"connected"}`,
+			mockStatus:    map[string]string{"status": "connected"},
+			mockStatusSet: true,
+			isJSON:        true,
+		},
+		{
+			arrayID:       "nonExistingArray",
+			expectedCode:  http.StatusNotFound,
+			expectedBody:  "array nonExistingArray not found \n",
+			mockStatusSet: false,
+			isJSON:        false,
+		},
+	}
+
+	for _, tt := range tests {
+		if tt.mockStatusSet {
+			probeStatus.Store(tt.arrayID, tt.mockStatus)
+		} else {
+			probeStatus.Delete(tt.arrayID)
+		}
+
+		req, err := http.NewRequest("GET", fmt.Sprintf("/arrayStatus/%s", tt.arrayID), nil)
+		assert.NoError(t, err)
+
+		rr := httptest.NewRecorder()
+		router.ServeHTTP(rr, req)
+
+		assert.Equal(t, tt.expectedCode, rr.Code)
+		if tt.isJSON {
+			assert.JSONEq(t, tt.expectedBody, rr.Body.String())
+		} else {
+			assert.Equal(t, tt.expectedBody, rr.Body.String())
+		}
+	}
 }
