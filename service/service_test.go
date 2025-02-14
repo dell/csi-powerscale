09db0f479ff795c171ad40f2d09b413668c0d9cf
sakshi-garg1 <74704849+sakshi-garg1@users.noreply.github.com>
2025-02-14 16:53:02 +0530

    Increasing UT coverage for service.go

--- service/service_test.go
+++ service/service_test.go
@@ -17,14 +17,18 @@ package service
 */
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net/http"
 	_ "net/http/pprof" // #nosec G108
 	"os"
+	"sync"
 	"testing"
 
 	"github.com/cucumber/godog"
 	"github.com/dell/csi-isilon/v2/common/constants"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestMain(m *testing.M) {
@@ -57,4 +61,108 @@ func TestMain(m *testing.M) {
 	fmt.Printf("status %d\n", status)
 
 	os.Exit(status)
+}
+
+func TestGetLoggerfunc(t *testing.T) {
+	ctx, _ := GetLogger(nil)
+	assert.Equal(t, nil, ctx)
+}
+
+func TestGetRunIDLogfunc(t *testing.T) {
+	ctx, _, _ := GetRunIDLog(nil)
+	assert.Equal(t, nil, ctx)
+}
+
+func TestGetIsiPathForVolumeFromClusterConfig(t *testing.T) {
+	o := Opts{
+		Path: "path",
+	}
+	s := service{
+		opts: o,
+	}
+
+	isilonConfig := IsilonClusterConfig{
+		IsiPath: "",
+	}
+	path := s.getIsiPathForVolumeFromClusterConfig(&isilonConfig)
+	assert.Equal(t, "path", path)
+
+	isilonConfig = IsilonClusterConfig{
+		IsiPath: "path/path",
+	}
+	path = s.getIsiPathForVolumeFromClusterConfig(&isilonConfig)
+	assert.Equal(t, "path/path", path)
+}
+
+func TestGetCSINodeIP(t *testing.T) {
+	s := service{
+		nodeIP: "",
+	}
+	ctx := context.Background()
+	_, err := s.GetCSINodeIP(ctx)
+	assert.Equal(t, errors.New("cannot get node IP"), err)
+}
+
+func TestGetCSINodeID(t *testing.T) {
+	s := service{
+		nodeID: "",
+	}
+	ctx := context.Background()
+	_, err := s.GetCSINodeID(ctx)
+	assert.Equal(t, errors.New("cannot get node id"), err)
+}
+
+func TestGetIsilonClusterLength(t *testing.T) {
+	tests := []struct {
+		name      string
+		setupFunc func(*sync.Map)
+		expected  int
+	}{
+		{
+			name: "Empty Map",
+			setupFunc: func(m *sync.Map) {
+				// No setup, map remains empty
+			},
+			expected: 0,
+		},
+		{
+			name: "Single Entry",
+			setupFunc: func(m *sync.Map) {
+				m.Store("cluster1", struct{}{})
+			},
+			expected: 1,
+		},
+		{
+			name: "Multiple Entries",
+			setupFunc: func(m *sync.Map) {
+				m.Store("cluster1", struct{}{})
+				m.Store("cluster2", struct{}{})
+				m.Store("cluster3", struct{}{})
+			},
+			expected: 3,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			serviceInstance := &service{
+				isiClusters: &sync.Map{},
+			}
+
+			tt.setupFunc(serviceInstance.isiClusters)
+			actual := serviceInstance.getIsilonClusterLength()
+
+			if actual != tt.expected {
+				t.Errorf("Expected %d, got %d", tt.expected, actual)
+			}
+		})
+	}
+}
+
+func TestGetNodeLabels(t *testing.T) {
+	s := service{
+		nodeID: "",
+	}
+	_, err := s.GetNodeLabels()
+	assert.NotEqual(t, nil, err)
 }
