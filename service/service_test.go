60c77953295ae5bf516b6eb221007d0752fd1ae5
sakshi-garg1 <74704849+sakshi-garg1@users.noreply.github.com>
2025-02-18 16:57:16 +0530

    Increasing coverage of service.go

--- service/service_test.go
+++ service/service_test.go
@@ -30,6 +30,7 @@ import (
 
 	"github.com/cucumber/godog"
 	"github.com/dell/csi-isilon/v2/common/constants"
+	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/stretchr/testify/mock"
@@ -224,6 +225,10 @@ func TestServiceInitializeServiceOpts(t *testing.T) {
 	assert.Equal(t, wantOps, serviceInstance.opts)
 	assert.Equal(t, wantEnvNodeName, serviceInstance.nodeID)
 	assert.Equal(t, wantEnvNodeIP, serviceInstance.nodeIP)
+
+	os.Unsetenv(constants.EnvIsilonConfigFile)
+	serviceInstance.initializeServiceOpts(ctx)
+	assert.Equal(t, wantEnvNodeIP, serviceInstance.nodeIP)
 }
 
 func TestSyncIsilonConfigs(t *testing.T) {
@@ -242,14 +247,14 @@ func TestGetNewIsilonConfigs(t *testing.T) {
 	tmpDir := t.TempDir()
 	isilonConfigFile := filepath.Join(tmpDir, isilonConfigFile)
 
-	// scenario 1
+	// scenario 1: invalid yaml
 	content := `isilonClusters:
   - clusterName=`
 	configBytes, err := writeToFileandRead(isilonConfigFile, content)
 	_, _, err = s.getNewIsilonConfigs(ctx, configBytes)
 	assert.NotEqual(t, nil, err)
 
-	// scenario 2
+	// scenario 2 : invalid cluster details
 	content = `isilon:
   - clusterName: "abc"`
 	configBytes, err = writeToFileandRead(isilonConfigFile, content)
@@ -296,7 +301,7 @@ func TestGetNewIsilonConfigs(t *testing.T) {
 	_, _, err = s.getNewIsilonConfigs(ctx, configBytes)
 	assert.NotEqual(t, nil, err)
 
-	// scenario 5
+	// scenario 5: empty username
 	content = `isilonClusters:
   - clusterName: "cluster1"
     username: ""
@@ -307,7 +312,7 @@ func TestGetNewIsilonConfigs(t *testing.T) {
 	_, _, err = s.getNewIsilonConfigs(ctx, configBytes)
 	assert.NotEqual(t, nil, err)
 
-	// scenario 6
+	// scenario 6 : empty password
 	content = `isilonClusters:
   - clusterName: "cluster1"
     username: "user"
@@ -318,7 +323,7 @@ func TestGetNewIsilonConfigs(t *testing.T) {
 	_, _, err = s.getNewIsilonConfigs(ctx, configBytes)
 	assert.NotEqual(t, nil, err)
 
-	// scenario 7
+	// scenario 7 : empty endpoint
 	content = `isilonClusters:
   - clusterName: "cluster1"
     username: "user"
@@ -328,6 +333,76 @@ func TestGetNewIsilonConfigs(t *testing.T) {
 	configBytes, err = writeToFileandRead(isilonConfigFile, content)
 	_, _, err = s.getNewIsilonConfigs(ctx, configBytes)
 	assert.NotEqual(t, nil, err)
+
+	// scenario 8 : endpoint with https
+	opt = Opts{
+		CustomTopologyEnabled:     false,
+		Port:                      "1234",
+		SkipCertificateValidation: true,
+		Path:                      "path",
+		IsiVolumePathPermissions:  "true",
+		IgnoreUnresolvableHosts:   false,
+		Verbose:                   uint(1),
+		isiAuthType:               uint8(1),
+	}
+	s = &service{
+		opts: opt,
+	}
+	content = `isilonClusters:
+  - clusterName: "cluster1"
+    username: "user"
+    password: "password"
+    endpoint: "https://1.2.3.4"
+    isDefault: true`
+	copy_noProbeOnStart := noProbeOnStart
+	noProbeOnStart = true
+	configBytes, err = writeToFileandRead(isilonConfigFile, content)
+	_, _, err = s.getNewIsilonConfigs(ctx, configBytes)
+	assert.Equal(t, nil, err)
+
+	// scenario 9 : without default value
+	content = `isilonClusters:
+  - clusterName: "cluster1"
+    username: "user"
+    password: "password"
+    endpoint: "https://1.2.3.4"`
+	configBytes, err = writeToFileandRead(isilonConfigFile, content)
+	_, _, err = s.getNewIsilonConfigs(ctx, configBytes)
+	assert.Equal(t, nil, err)
+
+	// scenario 10 : 2 default values
+	content = `isilonClusters:
+  - clusterName: "cluster1"
+    username: "user"
+    password: "password"
+    endpoint: "1.2.3.4"
+    isDefault: true
+  - clusterName: "cluster2"
+    username: "user"
+    password: "password"
+    endpoint: "1.2.3.4"
+    isDefault: true`
+	configBytes, err = writeToFileandRead(isilonConfigFile, content)
+	_, _, err = s.getNewIsilonConfigs(ctx, configBytes)
+	assert.NotEqual(t, nil, err)
+
+	// scenario 11 : same cluster name
+	content = `isilonClusters:
+  - clusterName: "cluster1"
+    username: "user"
+    password: "password"
+    endpoint: "1.2.3.4"
+    isDefault: true
+  - clusterName: "cluster1"
+    username: "user"
+    password: "password"
+    endpoint: "1.2.3.4"
+    isDefault: false`
+	configBytes, err = writeToFileandRead(isilonConfigFile, content)
+	_, _, err = s.getNewIsilonConfigs(ctx, configBytes)
+	assert.NotEqual(t, nil, err)
+
+	noProbeOnStart = copy_noProbeOnStart
 }
 
 func writeToFileandRead(filePath, content string) ([]byte, error) {
@@ -391,4 +466,36 @@ func TestLoadIsilonConfigs(t *testing.T) {
 
 	// Wait to let the watcher pick up the event
 	time.Sleep(1 * time.Second)
+}
+
+func TestSetRunIDContext(t *testing.T) {
+	ctx := context.Background()
+	runID := "test-run-123"
+	newCtx, _ := setRunIDContext(ctx, runID)
+	require.NotNil(t, newCtx)
+}
+
+func TestGetIsiClient(t *testing.T) {
+	o := Opts{
+		CustomTopologyEnabled: true,
+	}
+	s := service{
+		opts: o,
+	}
+	ctx := context.Background()
+	isiConfig := IsilonClusterConfig{}
+	logLevel := logrus.InfoLevel
+	_, err := s.GetIsiClient(ctx, &isiConfig, logLevel)
+	assert.NotEqual(t, nil, err)
+}
+
+func TestGetIsilonConfig(t *testing.T) {
+	// utils.GetLogger()
+	ctx := context.Background()
+	clusterName := ""
+	s := service{
+		defaultIsiClusterName: "",
+	}
+	_, err := s.getIsilonConfig(ctx, &clusterName)
+	assert.NotEqual(t, "nil", err)
 }
