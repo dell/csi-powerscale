1323f86a03f24c57aa81dcf3a411cee9be1deba7
Surya Gupta <surya.gupta1@dell.com>
2025-02-20 07:24:43 -0500

    add UT for synchronize and suspend in replication_test.go

--- service/replication_test.go
+++ service/replication_test.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"errors"
 	"testing"
 
 	isi "github.com/dell/goisilon"
@@ -118,5 +119,152 @@ func Test_failbackDiscardLocal(t *testing.T) {
 	}).Times(1)
 
 	err := failbackDiscardLocal(context.Background(), localIsiConfig, remoteIsiConfig, "vgstest-Five_Minutes", logrus.NewEntry(logrus.New()))
+	assert.NoError(t, err)
+}
+
+func Test_synchronize(t *testing.T) {
+	mockClient := &MockClient{}
+
+	// Create a new instance of the isiService struct
+	svc := &isiService{
+		endpoint: "http://localhost:8080",
+		client: &isi.Client{
+			API: mockClient,
+		},
+	}
+
+	localIsiConfig := &IsilonClusterConfig{
+		IsiPath: "/ifs/data",
+		isiSvc:  svc,
+	}
+	remoteIsiConfig := &IsilonClusterConfig{
+		IsiPath: "/ifs/data",
+		isiSvc:  svc,
+	}
+
+	ppName := "vgstest-Five_Minutes"
+
+	// Negative case - when policy sync failed
+	svc.client.API.(*MockClient).On("Get", anyArgs...).Return(errors.New("policy sync failed")).Run(nil).Times(1)
+	err := synchronize(context.Background(), localIsiConfig, remoteIsiConfig, ppName, logrus.NewEntry(logrus.New()))
+	assert.Error(t, err)
+
+	// Positive cases
+	svc.client.API.(*MockClient).Calls = nil
+	svc.client.API.(*MockClient).On("Get", anyArgs...).Return(nil).Run(func(args mock.Arguments) {
+		resp := args.Get(5).(**v11.Policies)
+		*resp = &v11.Policies{
+			Policy: []v11.Policy{
+				{
+					ID:      "test-id",
+					Name:    "test-name",
+					Enabled: true,
+				},
+			},
+		}
+	}).Times(1)
+	svc.client.API.(*MockClient).On("Get", anyArgs...).Return(nil).Run(func(args mock.Arguments) {
+		resp := args.Get(5).(**v11.Jobs)
+		*resp = &v11.Jobs{
+			Job: []v11.Job{
+				{
+					ID:     "test-id",
+					Action: "sync",
+				},
+			},
+		}
+	}).Times(2)
+
+	svc.client.API.(*MockClient).On("Post", anyArgs...).Return(nil).Run(nil).Times(1)
+	svc.client.API.(*MockClient).On("Get", anyArgs[0:6]...).Return(nil).Times(1)
+
+	svc.client.API.(*MockClient).On("Get", anyArgs...).Return(nil).Run(func(args mock.Arguments) {
+		resp := args.Get(5).(**v11.Policies)
+		*resp = &v11.Policies{
+			Policy: []v11.Policy{
+				{
+					ID:      "test-id",
+					Name:    "test-name",
+					Enabled: true,
+				},
+			},
+		}
+	}).Times(1)
+	svc.client.API.(*MockClient).On("Put", anyArgs...).Return(nil).Run(nil).Times(1)
+	svc.client.API.(*MockClient).On("Get", anyArgs...).Return(nil).Run(func(args mock.Arguments) {
+		resp := args.Get(5).(**v11.Jobs)
+		*resp = &v11.Jobs{
+			Job: []v11.Job{
+				{
+					ID: "test-id",
+				},
+			},
+		}
+	}).Times(1)
+
+	err = synchronize(context.Background(), localIsiConfig, remoteIsiConfig, ppName, logrus.NewEntry(logrus.New()))
+	assert.NoError(t, err)
+}
+
+func Test_suspend(t *testing.T) {
+	mockClient := &MockClient{}
+
+	// Create a new instance of the isiService struct
+	svc := &isiService{
+		endpoint: "http://localhost:8080",
+		client: &isi.Client{
+			API: mockClient,
+		},
+	}
+
+	localIsiConfig := &IsilonClusterConfig{
+		IsiPath: "/ifs/data",
+		isiSvc:  svc,
+	}
+	remoteIsiConfig := &IsilonClusterConfig{
+		IsiPath: "/ifs/data",
+		isiSvc:  svc,
+	}
+
+	ppName := "vgstest-Five_Minutes"
+
+	// Negative case - can't disable local policy
+	svc.client.API.(*MockClient).On("Get", anyArgs...).Return(errors.New("can't disable local policy")).Run(nil).Times(1)
+	err := suspend(context.Background(), localIsiConfig, remoteIsiConfig, ppName, logrus.NewEntry(logrus.New()))
+	assert.Error(t, err)
+
+	// Negative case - policy couldn't reach disabled condition
+	svc.client.API.(*MockClient).Calls = nil
+	svc.client.API.(*MockClient).On("Get", anyArgs...).Return(nil).Run(func(args mock.Arguments) {
+		resp := args.Get(5).(**v11.Policies)
+		*resp = &v11.Policies{
+			Policy: []v11.Policy{
+				{
+					ID:   "test-id",
+					Name: "test-name",
+				},
+			},
+		}
+	}).Times(1)
+	svc.client.API.(*MockClient).On("Get", anyArgs...).Return(errors.New("policy couldn't reach disabled condition")).Run(nil).Times(1)
+
+	err = suspend(context.Background(), localIsiConfig, remoteIsiConfig, ppName, logrus.NewEntry(logrus.New()))
+	assert.Error(t, err)
+
+	// Positive cases
+	svc.client.API.(*MockClient).Calls = nil
+	svc.client.API.(*MockClient).On("Get", anyArgs...).Return(nil).Run(func(args mock.Arguments) {
+		resp := args.Get(5).(**v11.Policies)
+		*resp = &v11.Policies{
+			Policy: []v11.Policy{
+				{
+					ID:   "test-id",
+					Name: "test-name",
+				},
+			},
+		}
+	}).Times(2)
+
+	err = suspend(context.Background(), localIsiConfig, remoteIsiConfig, ppName, logrus.NewEntry(logrus.New()))
 	assert.NoError(t, err)
 }
