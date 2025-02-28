ab3cb43e1d48ddb7361dee7af23cec431ed24ced
niranjan-n1 <niranjan.n1@dell.com>
2025-02-28 16:33:08 +0530

    fixed lint issues

--- service/controller.go
+++ service/controller.go
@@ -1749,8 +1749,20 @@ func (s *service) CreateSnapshot(
 	// snapshotName: name of the snapshot that need to be created
 	var (
 		snapshotNew isi.Snapshot
+		params      map[string]string
 		isiPath     string
 	)
+
+	params = req.GetParameters()
+	if _, ok := params[IsiPathParam]; ok {
+		if params[IsiPathParam] == "" {
+			isiPath = isiConfig.IsiPath
+		} else {
+			isiPath = params[IsiPathParam]
+		}
+	} else {
+		// use the default isiPath if not set in the storage class
+		isiPath = isiConfig.IsiPath
 
 	srcVolumeID, _, _, clusterName, err := utils.ParseNormalizedVolumeID(ctx, req.GetSourceVolumeId())
 	if err != nil {
