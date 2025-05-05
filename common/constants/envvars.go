ec6279da78a23278716859c6b1aa5308be1c00e1
Niranjan-N <niranjan.n1@dell.com>
2025-05-05 12:06:20 +0530

    updated comment

--- common/constants/envvars.go
+++ common/constants/envvars.go
@@ -106,6 +106,6 @@ const (
 	// EnvMetadataRetrieverEndpoint specifies the endpoint address for csi-metadata-retriever sidecar
 	EnvMetadataRetrieverEndpoint = "CSI_RETRIEVER_ENDPOINT"
 
-	// EnvCsiVolPrefix specifies the volume prefix for the names of PersistentVolumes created
+	// EnvCsiVolPrefix specifies the volume prefix used for the names of PersistentVolumes created
 	EnvCsiVolPrefix = "X_CSI_VOL_PREFIX"
 )
