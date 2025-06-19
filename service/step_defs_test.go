7f91f44efad12e48df052bbb450a42a01d7dec86
santhoshatdell <Santhosh.Lakshmanan@dell.com>
2025-06-19 21:51:12 +0000

    Fixed lint error

--- service/step_defs_test.go
+++ service/step_defs_test.go
@@ -354,7 +354,7 @@ func FeatureContext(s *godog.ScenarioContext) {
 	s.Step(`^I call NodeUnpublishVolume$`, f.iCallNodeUnpublishVolume)
 	s.Step(`^I call EphemeralNodeUnpublishVolume$`, f.iCallEphemeralNodeUnpublishVolume)
 	s.Step(`^a valid NodeUnpublishVolumeResponse is returned$`, f.aValidNodeUnpublishVolumeResponseIsReturned)
-	s.Step(`^I call CreateSnapshot "([^"]*)" "([^"]*)" "([^"]*)"$`, f.iCallCreateSnapshot)
+	s.Step(`^I call CreateSnapshot "([^"]*)" "([^"]*)"$`, f.iCallCreateSnapshot)
 	s.Step(`^a valid CreateSnapshotResponse is returned$`, f.aValidCreateSnapshotResponseIsReturned)
 	s.Step(`^I call DeleteSnapshot "([^"]*)"$`, f.iCallDeleteSnapshot)
 	s.Step(`^I call CreateVolumeFromSnapshot "([^"]*)" "([^"]*)"$`, f.iCallCreateVolumeFromSnapshot)
@@ -2008,7 +2008,7 @@ func getCreateSnapshotRequest(srcVolumeID, name string) *csi.CreateSnapshotReque
 	return req
 }
 
-func (f *feature) iCallCreateSnapshot(volName, srcVolumeID, name string) error {
+func (f *feature) iCallCreateSnapshot(srcVolumeID, name string) error {
 	f.createSnapshotRequest = getCreateSnapshotRequest(srcVolumeID, name)
 	req := f.createSnapshotRequest
 	f.createSnapshotResponse, f.err = f.service.CreateSnapshot(context.Background(), req)
