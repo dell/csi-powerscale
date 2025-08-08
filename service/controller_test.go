6e1de9e2e9eb8f21dc93d6ca93e6870973096663
OliverChen557 <135048646+OliverChen557@users.noreply.github.com>
2025-08-08 11:56:53 -0400

    UT for `service` package (#436)

--- service/controller_test.go
+++ service/controller_test.go
@@ -30,6 +30,8 @@ import (
 	v1 "github.com/dell/goisilon/api/v1"
 	v2 "github.com/dell/goisilon/api/v2"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"go.uber.org/mock/gomock"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
@@ -1351,4 +1353,165 @@ func TestGetCapacity(t *testing.T) {
 	_, err := s.GetCapacity(ctx, req)
 
 	assert.NotEqual(t, nil, err)
+}
+
+func TestControllerPublishVolume_MaxVolumesPerNode(t *testing.T) {
+	fmt.Println("TestControllerPublishVolume_MaxVolumesPerNode")
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	s := &service{
+		nodeID:                identifiers.DummyHostNodeID,
+		nodeIP:                "127.0.0.1",
+		defaultIsiClusterName: "system",
+		opts:                  Opts{MaxVolumesPerNode: 5},
+		isiClusters:           &sync.Map{},
+	}
+	mockClient := &isimocks.Client{}
+	isiConfig := &IsilonClusterConfig{
+		ClusterName: "system",
+		isiSvc: &isiService{
+			client: &isi.Client{
+				API: mockClient,
+			},
+		},
+	}
+	s.isiClusters.Store("system", isiConfig)
+
+	ctx := context.Background()
+	req := &csi.ControllerPublishVolumeRequest{
+		VolumeId: "k8s-e89c9d089e=_=_=19=_=_=csi0zone=_=_=system",
+		NodeId:   identifiers.DummyHostNodeID,
+		VolumeCapability: &csi.VolumeCapability{
+			AccessMode: &csi.VolumeCapability_AccessMode{Mode: csi.VolumeCapability_AccessMode_SINGLE_NODE_WRITER},
+			AccessType: &csi.VolumeCapability_Mount{Mount: &csi.VolumeCapability_MountVolume{}},
+		},
+	}
+
+	// Mock the necessary calls
+	isiConfig.isiSvc.client.API.(*isimocks.Client).ExpectedCalls = nil
+	isiConfig.isiSvc.client.API.(*isimocks.Client).On("Get", anyArgs[0:6]...).Return(nil).Run(func(args mock.Arguments) {
+		resp := args.Get(5).(**apiv1.GetIsiVolumeAttributesResp)
+		*resp = &apiv1.GetIsiVolumeAttributesResp{}
+	}).Once()
+	isiConfig.isiSvc.client.API.(*isimocks.Client).On("Get", anyArgs[0:6]...).Return(nil).Run(func(args mock.Arguments) {
+		resp := args.Get(5).(**apiv1.GetIsiExportsResp)
+		*resp = &apiv1.GetIsiExportsResp{
+			ExportList: []*apiv1.IsiExport{
+				{Clients: []string{"127.0.0.1", "127.0.0.1"}},
+				{Clients: []string{"127.0.0.1"}},
+				{Clients: []string{"127.0.0.1"}},
+				{Clients: []string{"127.0.0.1"}},
+				{Clients: []string{"127.0.0.1"}},
+			},
+		}
+	}).Once()
+	isiConfig.isiSvc.client.API.(*isimocks.Client).On("Get", anyArgs[0:6]...).Return(nil)
+	_, err := s.ControllerPublishVolume(ctx, req)
+	assert.NotNil(t, err)
+	assert.Equal(t, codes.InvalidArgument, status.Code(err))
+	assert.Contains(t, err.Error(), "maximum volume limit reached for node")
+
+	// Error Scenario
+	isiConfig.isiSvc.client.API.(*isimocks.Client).ExpectedCalls = nil
+	isiConfig.isiSvc.client.API.(*isimocks.Client).On("Get", anyArgs[0:6]...).Return(nil).Run(func(args mock.Arguments) {
+		resp := args.Get(5).(**apiv1.GetIsiVolumeAttributesResp)
+		*resp = &apiv1.GetIsiVolumeAttributesResp{}
+	}).Once()
+	isiConfig.isiSvc.client.API.(*isimocks.Client).On("Get", anyArgs[0:6]...).Return(fmt.Errorf("failed to get exports")).Once()
+	_, err = s.ControllerPublishVolume(ctx, req)
+	assert.NotNil(t, err)
+	assert.Equal(t, codes.InvalidArgument, status.Code(err))
+	assert.Contains(t, err.Error(), "failed to export count for node id")
+}
+
+func TestGetIpsFromAZNetworkLabel(t *testing.T) {
+	originalGetNodeLabelsWithName := getNodeLabelsWithNameFunc
+
+	after := func() {
+		getNodeLabelsWithNameFunc = originalGetNodeLabelsWithName
+	}
+
+	tests := []struct {
+		name        string
+		nodeID      string
+		azNetwork   string
+		nodeLabels  map[string]string
+		expectedIPs []string
+		expectedErr error
+	}{
+		{
+			name:      "successful execution",
+			nodeID:    "nodename=#=#=localhost=#=#=127.0.0.1",
+			azNetwork: "10.0.0.1/24",
+			nodeLabels: map[string]string{
+				"csi-isilon.dellemc.com/aznetwork-10.0.0.1_24": "192.168.1.1-192.168.1.2",
+			},
+			expectedIPs: []string{"192.168.1.1", "192.168.1.2"},
+		},
+		{
+			name:        "node ID parsing error",
+			nodeID:      "invalid-node-id",
+			azNetwork:   "10.0.0.1/24",
+			expectedErr: fmt.Errorf("node ID '%s' cannot match the expected '^(.+)=#=#=(.+)=#=#=(.+)$' pattern", "invalid-node-id"),
+		},
+		{
+			name:        "node labels retrieval error",
+			nodeID:      "nodename=#=#=localhost=#=#=127.0.0.1",
+			azNetwork:   "10.0.0.1/24",
+			expectedIPs: []string{},
+			expectedErr: fmt.Errorf("failed to match AZNetwork to get IPs for export %s", "10.0.0.1/24"),
+		},
+		{
+			name:      "no matching AZNetwork label",
+			nodeID:    "nodename=#=#=localhost=#=#=127.0.0.1",
+			azNetwork: "10.0.0.1/24",
+			nodeLabels: map[string]string{
+				"csi-isilon.dellemc.com/aznetwork-10.0.0.2_24": "192.168.1.1-192.168.1.2",
+			},
+			expectedIPs: []string{},
+			expectedErr: fmt.Errorf("failed to match AZNetwork to get IPs for export %s", "10.0.0.1/24"),
+		},
+		{
+			name:        "empty node labels",
+			nodeID:      "nodename=#=#=localhost=#=#=127.0.0.1",
+			azNetwork:   "10.0.0.1/24",
+			nodeLabels:  map[string]string{},
+			expectedIPs: []string{},
+			expectedErr: fmt.Errorf("failed to match AZNetwork to get IPs for export %s", "10.0.0.1/24"),
+		},
+		{
+			name:        "error in getIpsFromAZNetworkLabel",
+			nodeID:      "nodename=#=#=localhost=#=#=127.0.0.1",
+			expectedIPs: nil,
+			expectedErr: fmt.Errorf("failed in getIpsFromAZNetworkLabel"),
+		},
+	}
+
+	s := &service{}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			defer after()
+
+			getNodeLabelsWithNameFunc = func(_ *service) func(string) (map[string]string, error) {
+				if tt.name == "error in getIpsFromAZNetworkLabel" {
+					return func(string) (map[string]string, error) {
+						return nil, fmt.Errorf("failed in getIpsFromAZNetworkLabel")
+					}
+				}
+				return func(string) (map[string]string, error) {
+					return tt.nodeLabels, nil
+				}
+			}
+
+			ips, err := s.getIpsFromAZNetworkLabel(context.Background(), tt.nodeID, tt.azNetwork)
+			if (err != nil) != (tt.expectedErr != nil) || (err != nil && err.Error() != tt.expectedErr.Error()) {
+				t.Errorf("getIpsFromAZNetworkLabel() error = %v, expectedErr %v", err, tt.expectedErr)
+			}
+			if !reflect.DeepEqual(ips, tt.expectedIPs) {
+				t.Errorf("getIpsFromAZNetworkLabel() IPs = %v, expectedIPs %v", ips, tt.expectedIPs)
+			}
+		})
+	}
 }
